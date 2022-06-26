36cc189a38742dd1e85ab74948a92fc4c80601f5
Qweog <Alireza.sharifib@live.se>
2022-06-26 17:58:17 +0200

    Finished with neighbours

--- neighbours/src/Neighbours.java
+++ neighbours/src/Neighbours.java
@@ -7,8 +7,8 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.awt.*;
+import java.util.*;
 
 import static java.lang.Math.round;
 import static java.lang.Math.sqrt;
@@ -45,8 +45,29 @@ public class Neighbours extends Application {
     void updateWorld() {
         // % of surrounding neighbours that are like me
         double threshold = 0.7;
+        int size = getSize(world);
+        Actor[] getNeighbours;
+        Actor[] notSatisfied = new Actor[size / 2];
+        int increment = 0;
 
-        // TODO
+        for (int row = 0; row < world.length; row++) {
+            for (int col = 0; col < world.length; col++) {
+                if (world[row][col] != null) {
+                    {getNeighbours = searchNeighbours(world, row, col);
+                        if (threshold <= getThreshhold(getNeighbours, world[row][col]))
+                            world[row][col].isSatisfied = true;
+                        else {
+                            world[row][col].isSatisfied = false;
+                            notSatisfied[increment++] = world[row][col];
+                        }
+                    }
+                }
+            }
+        }
+
+        Actor[][] satisfied = notSatisfied(world);
+    isSatisfied(notSatisfied, satisfied);
+
     }
 
     // This method initializes the world variable with a random distribution of Actors
@@ -59,16 +80,90 @@ public class Neighbours extends Application {
         // %-distribution of RED, BLUE and NONE
         double[] dist = {0.25, 0.25, 0.50};
         // Number of locations (places) in world (must be a square)
-        int nLocations = 900;   // Should also try 90 000
-
-        // TODO
-
+        int nLocations = 90_000;   // Should also try 90 000
+        Actor[] arr = creation(nLocations, dist);
+        shuffle(arr);
+        world = origMatrix(arr);
 
         // Should be last
         fixScreenSize(nLocations);
     }
 
-    // TODO Many methods here, break down of init() and updateWorld()
+
+    public Actor[][] isSatisfied(Actor[] unSatisfied, Actor[][] satisfied) {
+        Random rand = new Random();
+
+        Actor[] noNull = Arrays.stream(unSatisfied).filter(Objects::nonNull).toArray(Actor[]::new);
+
+        if (noNull.length != 0) {
+            for (Actor nonSatisfied : noNull) {
+                while(true){
+                int randRow = rand.nextInt(satisfied.length);
+                int randCol = rand.nextInt(satisfied.length);
+                if (satisfied[randRow][randCol] == null) {
+                    satisfied[randRow][randCol] = nonSatisfied;
+                    break;
+                }}
+            }
+
+        }
+        return satisfied;
+    }
+
+
+    public Actor[][] notSatisfied(Actor[][] satisfied) {
+
+        for (int i = 0; i < satisfied.length; i++) {
+            for (int j = 0; j < satisfied.length; j++) {
+                if (satisfied[i][j] != null && !satisfied[i][j].isSatisfied) {
+                    satisfied[i][j] = null;
+                }
+            }
+        }
+        return satisfied;
+    }
+
+    public double getThreshhold(Actor[] neighbours, Actor color) {
+        int sameColor = 0;
+        int oppositeColor = 0;
+
+        for (Actor object : neighbours) {
+            if (object == null) {
+            }
+            else if (object.color == color.color)
+                sameColor += 1;
+            else
+                oppositeColor += 1;
+        }
+
+        return (double) sameColor / (sameColor + oppositeColor);
+
+    }
+
+    public Actor[] searchNeighbours(Actor[][] actor, int row, int col) {
+
+        int increment = 0;
+        Actor[] neighbours = new Actor[8];
+        int size = getSize(actor);
+
+        for (int i = row-1; i < row+2; i++) {
+            for (int j = col-1; j < col+2; j++) {
+                if (!isValidLocation(getSize(actor), i, j) || j == col && i == row || j > sqrt(size) - 1
+                        || j < 0 || i > sqrt(size) - 1 || i < 0) {
+                }else
+                    neighbours[increment++] = actor[i][j];
+            }
+        }
+        return neighbours;
+    }
+
+    public int getSize(Actor[][] arr) {
+        Actor[] array = Arrays.stream(arr)
+                .flatMap(Arrays::stream)
+                .toArray(Actor[]::new);
+        return array.length;
+    }
+
 
     // Check if inside world
     boolean isValidLocation(int size, int row, int col) {
@@ -77,7 +172,48 @@ public class Neighbours extends Application {
 
     // ----------- Utility methods -----------------
 
-    // TODO Method to change format of data, generate random etc.
+    /**
+     * converts a 2-D array to one dimensions.
+     **/
+    public Actor[] creation(int nLocations, double[] dist) {
+        Actor[] arr = new Actor[nLocations];
+
+        for (int i = 0; i < nLocations; i++) {
+            if (i < nLocations * dist[0])
+                arr[i] = new Actor(Color.PURPLE);
+            else if (i < nLocations * (dist[0] + dist[1]))
+                arr[i] = new Actor(Color.BLACK);
+            else
+                arr[i] = null;
+        }
+        return arr;
+    }
+
+    public Actor[] shuffle(Actor[] actors) {
+        Random random = new Random();
+        for (int i = 0; i < actors.length; i++) {
+            int j = random.nextInt(i + 1);
+            Actor temp = actors[i];
+            actors[i] = actors[j];
+            actors[j] = temp;
+        }
+        return actors;
+    }
+
+    public Actor[][] origMatrix(Actor[] arr) {
+        int oneDSize = (int)sqrt(arr.length);
+        Actor[][] arr2 = new Actor[oneDSize][oneDSize];
+        int index = 0;
+        for (int i = 0; i < oneDSize; i++) {
+            for (int j = 0; j < oneDSize; j++) {
+                arr2[i][j] = arr[index++];
+            }
+        }
+        return arr2;
+    }
+
+
+
 
     // ------- Testing -------------------------------------
 
@@ -97,8 +233,6 @@ public class Neighbours extends Application {
         out.println(!isValidLocation(size, -1, 0));
         out.println(!isValidLocation(size, 0, 3));
 
-        // TODO  More tests here. Implement and test one method at the time
-        // TODO Always keep all tests! Easy to rerun if something happens
 
 
 
@@ -123,7 +257,7 @@ public class Neighbours extends Application {
     }
 
     long lastUpdateTime;
-    final long INTERVAL = 450_000_000;
+    final long INTERVAL = 10_000_000;
 
 
     @Override
@@ -148,10 +282,12 @@ public class Neighbours extends Application {
             }
         };
 
+
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Simulation");
         primaryStage.show();
+        primaryStage.setResizable(false);
 
         timer.start();  // Start simulation
     }
